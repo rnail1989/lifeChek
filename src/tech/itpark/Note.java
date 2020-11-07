ada8e80cbd29e9cc2010e98afef2ed37bd69f4ae
rnail1989 <rnail1989@mail.ru>
2020-11-07 09:16:19 +0300

    initail commit

---
+++ src/tech/itpark/Note.java
@@ -0,0 +1,48 @@
+package tech.itpark;
+
+public class Note {
+    private int id;
+    private String field;
+    private double cost;
+    private long created;
+
+    public Note(int id, String field, double cost, long created) {
+        this.id = id;
+        this.field = field;
+        this.cost = cost;
+        this.created = created;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public long getCreated() {
+        return created;
+    }
+
+    public void setCreated(long created) {
+        this.created = created;
+    }
+}
+
---
+++ src/tech/itpark/NoteManager.java
@@ -0,0 +1,5 @@
+package tech.itpark;
+
+public class NoteManager {
+
+}
